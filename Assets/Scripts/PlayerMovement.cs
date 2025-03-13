5fd2fd87b9ed149ba9be3cd66548cf44b8339971
LucasMo007 <easoncoldplay2@gmail.com>
2025-03-13 16:40:08 -0400

    make movement and Run animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,20 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using System;
+using UnityEditor.Tilemaps;
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField]  float runSpeed = 10f;
+    [SerializeField] float runSpeed = 3f;
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
+    Animator myAnimator;
 
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     void Update()
     {
         Run();
+        FlipSprite();
     }
     void OnMove(InputValue value)
     {
@@ -24,10 +28,21 @@ public class PlayerMovement : MonoBehaviour
         Debug.Log(moveInput);
 
     }
-   void Run()
-    {  
+    void Run()
+    {
         Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.velocity.y);
         myRigidbody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isRunning ", playerHasHorizontalSpeed);
+    }
+        void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
+        }
     }
 }
 
